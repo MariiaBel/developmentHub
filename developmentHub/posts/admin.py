641b5502dda4c1ecedfd869ced91b80183ef5287
MariiaBel <stepina.m.p@gmail.com>
2020-12-08 14:38:48 +0300

    add posts, static and templates

---
+++ developmentHub/posts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("pk", "text", "pub_date", "author") 
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",) 
+    # добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",) 
+
+
+admin.site.register(Post, PostAdmin)
