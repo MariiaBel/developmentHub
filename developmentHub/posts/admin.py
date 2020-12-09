bf2fefab3f54ae7e0e503b3a48a62d873bd553c3
MariiaBel <stepina.m.p@gmail.com>
2020-12-09 22:12:41 +0300

    add group

--- developmentHub/posts/admin.py
+++ developmentHub/posts/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Group
 
 class PostAdmin(admin.ModelAdmin):
     # перечисляем поля, которые должны отображаться в админке
-    list_display = ("pk", "text", "pub_date", "author") 
+    list_display = ("pk", "text", "group", "pub_date", "author") 
     # добавляем интерфейс для поиска по тексту постов
     search_fields = ("text",) 
     # добавляем возможность фильтрации по дате
     list_filter = ("pub_date",) 
+    empty_value_display = "-пусто-" 
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "description")
+    empty_value_display = "-пусто-" 
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
